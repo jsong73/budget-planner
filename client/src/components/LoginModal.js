ca3f4f898d2a8d3b28c7819cdcb93e005caf39a5
jsong73 <jessicajinsong@gmail.com>
2023-04-21 16:51:31 -0400

    imported Auth

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,10 +1,74 @@
 import React from "react";
 
-function LoginModal() {
+const LoginModal = (props) => {
+
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+      });
+    
+      const [login, {error}] = useMutation(LOGIN_USER);
+    
+      const handleChange = (event) => {
+        const {name, value} = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+    
+      const loginFormHandler = async (event) => {
+        event.preventDefault();
+        try{
+          const { data } = await login({
+            variables: {...formState}
+          });
+          console.log(data)
+          Auth.login(data.login.token);
+        } catch (error) {
+          console.log(error);
+        }
+    
+        setFormState({
+          email: "",
+          password: "",
+        });
+      };
+
   return (
     <div>
 
      <h1>Login</h1>
+        
+         <form onSubmit={loginFormHandler}>
+
+            <label> Email </label>
+              <input
+               placeholder="Email"
+               name="email"
+               type="email"
+               value={formState.email}
+               autoComplete="off"
+               onChange={handleChange} />
+
+
+              <label> Password </label>
+              <input
+               name="password"
+               type="password"
+               value={formState.password}
+               autoComplete="off"
+               onChange={handleChange} />
+
+
+              
+            {error ? (
+              <div> {error.message} </div>
+            ) : null}
+            <button type="submit"> Submit </button>
+
+          </form>
     </div>
   )}
 
