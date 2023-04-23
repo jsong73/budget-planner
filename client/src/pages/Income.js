638c8338c9981f3d5892a6358bc834ae0fbb28b0
jsong73 <jessicajinsong@gmail.com>
2023-04-23 18:08:01 -0400

    get income

--- client/src/pages/Income.js
+++ client/src/pages/Income.js
@@ -1,27 +1,41 @@
 import IncomeForm from "../components/IncomeForm"
 import IncomeDetails from "../components/IncomeDetails"
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries"
 
 
-function Income(incomes) {
+function Income() {
+
+  const {loading, data} = useQuery(QUERY_ME)
+  
+  console.log("loading:", loading);
+  console.log("data:", data);
+
+
+  if (loading) {
+    return <div> loading... </div>;
+}
+
+  const incomes = data?.me?.income || [];
+  console.log(incomes)
 
   return (
     <div className="flex justify-center items-center mt-12 ">
         <h1 className="font-bold text-3xl">Income</h1>
           <div>         
             <IncomeForm />
-
+{/* 
             {incomes.map((income) => {
-              const {_id, title, amount, date, category, description} = income;
+              const {_id, title, amount, date, description} = income;
               return <IncomeDetails
               key={_id}
               id={_id}
               title={title}
               amount={amount}
               date={date}
-              category={category}
               description={description}
               />
-            })}
+            })} */}
           </div>
  
     </div>
