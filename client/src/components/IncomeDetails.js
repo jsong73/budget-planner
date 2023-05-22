4b9851fb944503c5f92302315b3f9d2ae426c4ca
jsong73 <jessicajinsong@gmail.com>
2023-05-22 15:25:19 -0400

    mobile

--- client/src/components/IncomeDetails.js
+++ client/src/components/IncomeDetails.js
@@ -45,35 +45,35 @@ const removeIncomeHandler = async ( incomeId ) => {
 };
 
   return (
-  <div className="flex w-full h-32 ml-80">
-    <div className="border border-white rounded-lg p-4 mb-4 flex w-screen">
+  <div className="flex w-full h-32 ml-80 ">
+    <div className="border border-white rounded-lg p-4 mb-4 flex flex-wrap w-screen">
 
       <div className="flex items-center">
         {profit}
-          <h1 className="font-bold ml-1 mr-2 text-xs lg:text-xl"> {title} </h1>
+          <h1 className="font-bold ml-1 mr-2 text-sm lg:text-xl"> {title} </h1>
       </div>
 
 
       <div className="flex items-center">
         {money}
-        <div className="ml-1 mr-2 text-xs lg:text-xl"> ${amount} </div>
+        <div className="ml-1 mr-2 text-sm lg:text-xl"> ${amount} </div>
       </div>
 
 
     <div className="flex items-center">
       {calender} 
-        <div className="ml-1 mr-2 text-xs lg:text-xl">{date}</div>
+        <div className="ml-1 mr-2 text-sm lg:text-xl">{date}</div>
     </div>
    
 
     <div className="flex items-center">
       {detail} 
-        <div className="ml-1 text-xs lg:text-xl">{description}</div>
+        <div className="ml-1 text-sm lg:text-xl">{description}</div>
     </div>
     
 
     <button 
-      className="flex ml-auto text-sm lg:text-2xl"
+      className="flex ml-auto text-md lg:text-2xl"
       onClick={() => removeIncomeHandler(id)}> 
       {deleteBtn}
     </button>
