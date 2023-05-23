fb76dd0b757becd63f765319cff530262876d8cf
jsong73 <jessicajinsong@gmail.com>
2023-05-22 22:45:02 -0400

    deployment lines

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require("apollo-server-express")
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,6 +18,15 @@ const PORT = process.env.PORT || 3009;
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// add below two lines for deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+  
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
